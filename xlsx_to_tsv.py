e92e19eb7328859db932e7521a68b902524e8bb5
paulocilasjr <pj_ibm@hotmail.com>
2024-10-11 15:05:14 -0400

    Get each tab dataset

--- xlsx_to_tsv.py
+++ xlsx_to_tsv.py
@@ -1,52 +1,62 @@
 import os
 import pandas as pd
 
-directory = 'dataset_inputs'
-if not os.path.exists(directory):
-    os.makedirs(directory)
+# Set directory and create if not present
+DIRECTORY = 'dataset_inputs'
+os.makedirs(DIRECTORY, exist_ok=True)
 
-def truncate_values(dataframe):
-    '''truncate extreme values for tmb, age, and nlr'''
-    dataframe.loc[:, 'TMB'] = dataframe['TMB'].clip(upper=50)
-    dataframe.loc[:, 'Age'] = dataframe['Age'].clip(upper=85)
-    dataframe.loc[:, 'NLR'] = dataframe['NLR'].clip(upper=25)
-    print('truncate completed')
-    return dataframe
+# Function to truncate extreme values
+def truncate_values(df):
+    '''Truncate extreme values for TMB, Age, and NLR using .loc to avoid SettingWithCopyWarning'''
+    df.loc[:, 'TMB'] = df['TMB'].clip(upper=50)
+    df.loc[:, 'Age'] = df['Age'].clip(upper=85)
+    df.loc[:, 'NLR'] = df['NLR'].clip(upper=25)
+    print('Truncation completed')
+    return df
 
-def save_in_file(tsv_file, tsv_file_name):
-    '''save tsv file dataset'''
-    tsv_file.to_csv(tsv_file_name, sep='\t', index=False)
-    print(f'{tsv_file_name}: save completed')
+# Function to save DataFrame as TSV file
+def save_to_tsv(df, file_path):
+    '''Save dataframe as TSV file'''
+    df.to_csv(file_path, sep='\t', index=False)
+    print(f'{file_path}: Save completed')
 
-def read_sheet(data_file, sheet_name):
-    dataframe = pd.read_excel(data_file, sheet_name=sheet_name, index_col=0)
-    print('read sheet completed')
-    return dataframe 
+# Function to read a specific sheet from Excel file
+def read_sheet(file_path, sheet_name):
+    '''Read an Excel sheet into a DataFrame'''
+    df = pd.read_excel(file_path, sheet_name=sheet_name, index_col=0)
+    print(f'{sheet_name}: Sheet read completed')
+    return df
 
-def column_selections(selected_columns, dataframe):
-    dataframe = dataframe.loc[:, selected_columns]
-    dataframe = truncate_values(dataframe)
-    print('column selection completed')
-    return dataframe
+# Function to select and truncate specific columns
+def select_and_truncate_columns(selected_columns, df):
+    '''Select specific columns and truncate extreme values'''
+    df_selected = df.loc[:, selected_columns].copy()  # Use .copy() to avoid SettingWithCopyWarning
+    df_selected = truncate_values(df_selected)
+    print('Column selection completed')
+    return df_selected
 
-features = ['TMB', 'Systemic_therapy_history', 'Albumin', 'NLR', 'Age'] + \
-          [f'CancerType{i}' for i in range(1, 17)]
-
-features_response = ['TMB', 'Systemic_therapy_history', 'Albumin', 'NLR', 'Age'] + \
-                   [f'CancerType{i}' for i in range(1, 17)] + ['Response']
+# Function to select feature columns based on response presence
+def get_feature_columns(response_present):
+    '''Return selected columns based on response presence'''
+    common_features = ['TMB', 'Systemic_therapy_history', 'Albumin', 'NLR', 'Age'] + \
+                      [f'CancerType{i}' for i in range(1, 17)]
+    
+    if response_present == 'Response':
+        return common_features + ['Response']
+    
+    return common_features
 
 # Load and preprocess training data
-data_all = './dataset/AllData.xlsx'
+ALL_DATA_PATH = './dataset/AllData.xlsx'
+sheets = ['Chowell_train', 'Chowell_test', 'MSK1']
 
-list_of_sheets = ['Chowell_train', 'Chowell_test', 'MSK1']
+for sheet in sheets:
+    df = read_sheet(ALL_DATA_PATH, sheet)
 
-for sheet in list_of_sheets:
-    dataframe = read_sheet(data_all, sheet)
-    
-    dataframe_no_response = column_selections(features, dataframe)
-    file_name = f'{directory}/{sheet}_without_response.tsv'
-    save_in_file(dataframe_no_response, file_name)
-    
-    dataframe_response = column_selections(features_response, dataframe)
-    file_name = f'{directory}/{sheet}_with_response.tsv'
-    save_in_file(dataframe_response, file_name)
+    for feature_option in ['Response', 'No_Response']:
+        selected_columns = get_feature_columns(feature_option)
+        df_selected = select_and_truncate_columns(selected_columns, df)
+        
+        # Save processed data to file
+        file_path = os.path.join(DIRECTORY, f'{sheet}_{feature_option}.tsv')
+        save_to_tsv(df_selected, file_path)
